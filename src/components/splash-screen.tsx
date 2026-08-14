fb8088cded9650f1422fbc62c082392d017c20fc
Yonas Asfaw <yonas2754@gmail.com>
2026-08-14 23:05:03 +0300

    Add admin deposit dashboard

--- src/components/splash-screen.tsx
+++ src/components/splash-screen.tsx
@@ -10,7 +10,7 @@ export default function SplashScreen() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowSplash(false);
-    }, 3000);
+    }, 1000);
 
     return () => clearTimeout(timer);
   }, []);
